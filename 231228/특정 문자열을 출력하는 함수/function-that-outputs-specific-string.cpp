f0523a8df3123d6cc6334d547774b40c1498db70
Moon <59304977+mun9769@users.noreply.github.com>
2023-12-28 09:26:57 +0900

    Title: 특정 문자열을 출력하는 함수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231228/특정 문자열을 출력하는 함수/function-that-outputs-specific-string.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+void print_func() {
+    cout << "Hello" << '\n';
+
+cout << "#@#@#@#@#@" << '\n';
+cout << "CodeTree" << '\n';
+cout << "@#@#@#@#@#" << '\n';
+cout << "Students!" << '\n';
+cout << '\n';
+}
+int n;
+int main() {
+    cin >> n;
+    for(int i=0; i<n; i++) print_func();
+    return 0;
+}
